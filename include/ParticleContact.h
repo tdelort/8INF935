80b19a0abbed9b4068bc90c544579e97229a2ca0
tdelort <tristan.delort@gmail.com>
2021-10-18 10:53:34 -0400

    added ParticleContact.h

--- include/ParticleContact.h
+++ include/ParticleContact.h
@@ -1,7 +1,23 @@
 #pragma once
 
+#include "Particle.h"
+#include "Vector3D.h"
+
 class ParticleContact
 {
 public:
 
+    Particle* particle[2];
+
+    float restitution;
+    float penetration;
+    Vector3D contactNormal;
+
+    void resolve(float duration);
+    float calculateSeparatingVelocity();
+
+private:
+
+    void resolveVelocity();
+    void resolveInterpenetration();
 };
