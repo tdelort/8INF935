a72253c379dabc65a8097fea2ef9e6ae82e18e8b
wajihalaoui <wajihalaoui@gmail.com>
2021-10-18 09:44:00 -0400

    Test

---
+++ include/ParticleDrag.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Particle.h"
+#include "ParticleForceGenerator.h"
+#include "Vector3D.h"
+
+class ParticleDrag: public ParticleForceGenerator
+{
+private:
+// drag coeffiecents
+    float m_k1;
+    float m_k2;
+public:
+    void UpdateForce (Particle* particle, float duration);
+// apply simplified drag based on particle's velocity 
+}
