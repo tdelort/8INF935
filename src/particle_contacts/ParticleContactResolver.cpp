013db37da529081080eb252c05989847659b9cac
Lucas <lucas.miras@live.fr>
2021-10-24 18:32:48 -0400

    fix resolver

--- src/particle_contacts/ParticleContactResolver.cpp
+++ src/particle_contacts/ParticleContactResolver.cpp
@@ -4,12 +4,12 @@
 
 void ParticleContactResolver::resolveContacts(std::vector<ParticleContact*>* contacts, float duration)
 {
-	//On r�soud le contact avec la plus petite velocit� relative en premier 
+	//On resoud le contact avec la plus petite velocite relative en premier 
 	if (contacts->empty())
 		return;
 	float sRelativeVelocity = contacts->at(0)->calculateSeparatingVelocity();
 	int iContact = 0, nbIteration = 0;
-	while (nbIteration < 2) { //deux it�rations de la liste possible
+	while (nbIteration < contacts->size( )  { 
 		for (int i = 0; i < contacts->size(); i++)
 		{
 			float iRelativeVelocity = contacts->at(i)->calculateSeparatingVelocity();
