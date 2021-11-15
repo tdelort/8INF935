0715d3b6dd7f6fae74243c56c1b73d86b34d39ea
Lucas <lucas.miras@live.fr>
2021-11-15 09:59:29 -0500

    Add force, forceAtPoint and forceAtbodyPoint
    ClearAccumulator and local to world position

--- include/RigidBody.h
+++ include/RigidBody.h
@@ -1,17 +1,31 @@
 #pragma once
 
 #include "Vector3D.h"
+#include "Quaternion.h"
+#include "Matrix3x4.h"
 
 class RigidBody
 {
     private:
+    float m_invMass;
     float m_angularDamping;
+    Vector3D m_velocity;
+    Vector3D m_position;
+
+    Quaternion m_orientation;
+
+    Vector3D m_rotation;
+
+    Matrix3x4 m_transformMatrix;
 
     Vector3D m_forceAccum;
 
     Vector3D m_torqueAccum;
 
     public:
+
+    void Integrate(float duration);
+
     void AddForce(const Vector3D& force);
 
     void AddForceAtPoint(const Vector3D& force, const Vector3D& pointMonde);
@@ -20,4 +34,8 @@ class RigidBody
 
     void ClearAccum();
 
+    private:
+
+    Vector3D worldPosition(const Vector3D& local)
+    
 };
---
+++ src/RigidBody.cpp
@@ -0,0 +1,33 @@
+#include "RigidBody.h"
+
+void RigidBody::Integrate(float duration)
+{
+    
+}
+void RigidBody::AddForce(const Vector3D& force)
+{
+    m_forceAccum =+ force;
+}
+
+void RigidBody::addForceAtPoint(const Vector3D& force, const Vector3D& pointMonde)
+{
+    addForce(force);
+    m_torqueAccum += (m_position - pointMonde) * force;
+}
+
+void RigidBody::AddForceAtBodyPoint(const Vector3D& force, const Vector3D& pointLocal)
+{
+    Vector3D world = WorldPosition(pointLocal);
+    AddForceAtPoint(force, wolrd);
+}
+
+void RigidBody::ClearAccumulator()
+{
+    m_forceAccum = Vector3D(0,0,0);
+    m_torqueAccum = Vector3D(0,0,0);
+}
+
+Vector3D RigidBody::WorldPosition(const Vector3D& local)    
+{
+    return m_transformMtrix.Inverse()*local;
+}
