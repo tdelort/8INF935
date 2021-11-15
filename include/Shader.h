7eaff8bb3034ca4f54f0a4d825fa5bb884b20b1b
tdelort <tristan.delort@gmail.com>
2021-11-15 10:45:32 -0500

    New OpenGL version and Shaders

--- include/Shader.h
+++ include/Shader.h
@@ -4,37 +4,60 @@
 
 //Vertex Shader
 const char* vertexSource = R"glsl(
-    #version 130
+    #version 150
 
 	in vec3 position;
-    in vec3 color;
-
-    out vec3 Color;
 
 	uniform mat4 model;
 	uniform mat4 view;
 	uniform mat4 proj;
-    uniform vec3 albedo;
 
 	void main()
 	{
-        //Color = color;
-        Color = albedo;
 		gl_Position = proj * view * model * vec4(position, 1.0);
 	}
 )glsl";
 
+//Geometry Shader
+const char* geometrySource = R"glsl(
+    #version 150
+
+    layout(triangles) in;
+    layout(triangle_strip, max_vertices=3) out;
+
+    out vec3 normal;
+
+    void main( void )
+    {
+        vec3 a = ( gl_in[1].gl_Position - gl_in[0].gl_Position ).xyz;
+        vec3 b = ( gl_in[2].gl_Position - gl_in[0].gl_Position ).xyz;
+        vec3 N = normalize( cross( b, a ) );
+
+        for( int i=0; i<gl_in.length(); ++i )
+        {
+            gl_Position = gl_in[i].gl_Position;
+            normal = N;
+            EmitVertex();
+        }
+
+        EndPrimitive();
+    }
+)glsl";
+
 //Fragment Shader
 const char* fragmentSource = R"glsl(
-    #version 130
+    #version 150
 
-    in vec3 Color;
+    uniform vec3 albedo;
+    in vec3 normal;
+
+    vec3 lightPos = vec3(0.0, 0.0, 0.0);
 
 	out vec4 outColor;
 
 	void main()
 	{
-		outColor = vec4(Color, 1.0);
+		outColor = vec4(normal, 1.0);
 	}
 )glsl";
 
@@ -49,6 +72,14 @@ GLuint createProgram()
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
     std::cout << "Vertex shader compile status : " << status << std::endl;
 
+    // Creating Geometry shader
+    GLuint geometryShader = glCreateShader(GL_GEOMETRY_SHADER);
+    glShaderSource(geometryShader, 1, &geometrySource, NULL);
+    glCompileShader(geometryShader);
+
+    glGetShaderiv(geometryShader, GL_COMPILE_STATUS, &status);
+    std::cout << "Geometry shader compile status : " << status << std::endl;
+
     // Creating Fragment shader
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
@@ -60,6 +91,7 @@ GLuint createProgram()
     // Creating shader program and linking it
     GLuint program = glCreateProgram();
     glAttachShader(program, vertexShader);
+    glAttachShader(program, geometryShader);
     glAttachShader(program, fragmentShader);
     glLinkProgram(program);
     glUseProgram(program);
