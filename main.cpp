5333b68126ffbd4206f070252450593af69a7fb3
tdelort <tristan.delort@gmail.com>
2021-11-21 17:50:52 -0500

    command line arg

--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 
 int main(int argc, char** argv)
 {
-    Demo demo("../../teapot.obj");
+    if(argc < 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <mesh_path>" << std::endl;
+        return 1;
+    }
+    std::cout << "using mesh : " << argv[1] << std::endl;
+    Demo demo(argv[1]);
     demo.run();
     return 0;
 }
